951b1c646db8160fa17e8d57c02b91000898960c
Andre Sch <andresch.dev@gmail.com>
2024-02-18 16:08:01 -0300

    Fix Gson null handling

--- server/src/main/java/com/forum/http/impl/Javalin/GsonMapper.java
+++ server/src/main/java/com/forum/http/impl/Javalin/GsonMapper.java
@@ -2,10 +2,11 @@ package com.forum.http.impl.Javalin;
 
 import java.lang.reflect.Type;
 import io.javalin.json.JsonMapper;
+import com.google.gson.GsonBuilder;
 import com.google.gson.Gson;
 
 class GsonMapper implements JsonMapper {
-  private Gson gson = new Gson();
+  private Gson gson = new GsonBuilder().serializeNulls().create();
 
   public String toJsonString(Object obj, Type type) {
     return gson.toJson(obj, type);
