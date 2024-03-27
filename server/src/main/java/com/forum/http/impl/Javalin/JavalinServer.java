e13b6ced22e557a77f4197f1636ca7efb0dc38ee
Andre Sch <andresch.dev@gmail.com>
2024-03-27 14:35:17 -0300

    Allow resource access from any origin

--- server/src/main/java/com/forum/http/impl/Javalin/JavalinServer.java
+++ server/src/main/java/com/forum/http/impl/Javalin/JavalinServer.java
@@ -8,9 +8,14 @@ public class JavalinServer implements HttpServer {
   private GsonMapper gsonMapper = new GsonMapper();
 
   public HttpApp start(int port) {
-    Javalin app = Javalin.create(
-      config -> config.jsonMapper(gsonMapper)
-    ).start(port);
+    Javalin app = Javalin.create(config -> {
+      config.jsonMapper(gsonMapper);
+
+      config.plugins.enableCors(cors -> {
+        cors.add(corsConfig -> corsConfig.anyHost());
+      });
+    }).start(port);
+
     return new JavalinApp(app);
   }
 }
